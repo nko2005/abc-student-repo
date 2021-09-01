e88c45536f88abc20463dcc5551deea53d9ade79
nko2005 <nko2005@nyu.edu>
2021-09-01 19:45:44 +0300

    Finalizing the project

--- projects/coding-foundation/script.js
+++ projects/coding-foundation/script.js
@@ -1,12 +1,31 @@
-const the_text = document.getElementById('_num_');
+let the_text = document.getElementById('_num_');
 const the_button = document.getElementById("_btn_");
+
+const clear_button = document.getElementById("_clr_btn_");
+let square_area = document.getElementById("square-space")
 the_button.innerHTML="don't click me!";
+
+clear_button.addEventListener("click",function(){
+  square_area.innerHTML="";
+
+
+});
 
 
 the_button.addEventListener("click",function(){
 
-  const the_input = the_text.value;
-  console.log(the_input,"fff");
+  let the_input = parseInt(the_text.value);
+
+  for (let i =0; i<the_input; i++){
+    let box = document.createElement("div")
+
+
+    let textnode = document.createTextNode(i+"");
+    box.appendChild(textnode);
+    square_area.appendChild(box);
+    //square_area.appendChild(document.createElement("button"));
+
+  }
 
 
 
