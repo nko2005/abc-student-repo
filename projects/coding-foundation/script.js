d8d52a7eaab6a3ff8cfa0ba2fad79ca15a30c478
nko2005 <nko2005@nyu.edu>
2021-09-01 20:10:43 +0300

    Adding polish

--- projects/coding-foundation/script.js
+++ projects/coding-foundation/script.js
@@ -1,29 +1,30 @@
+
+//Grabbing the html elements
 let the_text = document.getElementById('_num_');
 const the_button = document.getElementById("_btn_");
-
 const clear_button = document.getElementById("_clr_btn_");
 let square_area = document.getElementById("square-space")
-the_button.innerHTML="don't click me!";
 
+//Event for the reset button to clear the div
 clear_button.addEventListener("click",function(){
   square_area.innerHTML="";
 
 
 });
 
-
+//Event for when submiting the number entered
 the_button.addEventListener("click",function(){
 
   let the_input = parseInt(the_text.value);
 
-  for (let i =0; i<the_input; i++){
+  for (let i =1; i<=the_input; i++){
     let box = document.createElement("div")
 
 
     let textnode = document.createTextNode(i+"");
     box.appendChild(textnode);
     square_area.appendChild(box);
-    //square_area.appendChild(document.createElement("button"));
+    
 
   }
 
