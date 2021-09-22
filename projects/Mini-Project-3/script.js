65124b4536b306204c1d577560bf7f70caaa8df2
nko2005 <nko2005@nyu.edu>
2021-09-22 17:53:40 +0300

    Changed background color

--- projects/Mini-Project-3/script.js
+++ projects/Mini-Project-3/script.js
@@ -3,12 +3,12 @@ let button = document.getElementById('button');
 let button2 = document.getElementById('troll');
 
 button.addEventListener("click",()=>{
-  document.body.style.backgroundColor= "black";
+  document.body.style.backgroundColor= "Green";
 
 })
 
 button2.addEventListener("click",()=>{
   document.body.style.backgroundImage= "url(troll.png)";
-  console.log("test");
+
 
 })
