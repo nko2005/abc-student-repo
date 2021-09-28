c648021f793733eef1e1a413fbe518dc7aec4e77
nko2005 <nko2005@nyu.edu>
2021-09-28 21:30:06 +0300

    Work

--- projects/Project-A/script.js
+++ projects/Project-A/script.js
@@ -1,4 +1,4 @@
-console.log("Cookies: " + navigator.cookieEnabled);
+/*
 console.log("Browser Language: " + navigator.browserLanguage);
 console.log("Language: " + navigator.language);
 console.log("Platform: " + navigator.platform);
@@ -10,20 +10,17 @@ console.log("extra" + navigator.userAgentData);
 
 console.log("device" + window.navigator.userAgent);
 
-/*
+*/
 if ("geolocation" in navigator) {
     // Do something with coordinates returned
     function processCoords(position) {
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
-        let first_div = document.querySelector('div');
-        let el_h2 = document.createElement('h2');
-
+        console.log(latitude,longitude);
         // Set h2 text as coordinates
-        el_h2.innerText = `Latitude: ${latitude}, Longitude: ${longitude}`;
+
 
         // Append h2 to document
-        first_div.appendChild(el_h2);
 
 
         // Fetch Coordinates
@@ -31,29 +28,45 @@ if ("geolocation" in navigator) {
 
 
 
-
-    const KEY = "AIzaSyD8LFh53VddzDevOC6A5Jhln9KgpmpoExg";
+    // /const KEY = "AIzaSyD8LFh53VddzDevOC6A5Jhln9KgpmpoExg";
     const ActualKeylol = "AIzaSyBdbFJuSYodgUsF2F2wz7fdm56b97tkr50";
     const LAT =  latitude;
     const LNG = longitude;
     let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=${ActualKeylol}`;
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      let parts = data.results[0].address_components;
-      document.body.insertAdjacentHTML("beforeend",`<p>Formatted: ${data.results[0].formatted_address}</p>`);
-    parts.forEach(part => {
-      if (part.types.includes("country")) {
-        //we found "country" inside the data.results[0].address_components[x].types array
-        document.body.insertAdjacentHTML(
-          "beforeend",
-          `<p>COUNTRY: ${part.long_name}</p>`
-        );
-      }
+
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          let parts = data.results[0].address_components;
+          document.body.insertAdjacentHTML(
+            "beforeend",
+            `<p>Formatted: ${data.results[0].formatted_address}</p>`
+          );
+          parts.forEach(part => {
+            if (part.types.includes("country")) {
+              //we found "country" inside the data.results[0].address_components[x].types array
+              document.body.insertAdjacentHTML(
+                "beforeend",
+                `<p>COUNTRY: ${part.long_name}</p>`
+              );
+            }
+            if (part.types.includes("administrative_area_level_1")) {
+              document.body.insertAdjacentHTML(
+                "beforeend",
+                `<p>PROVINCE: ${part.long_name}</p>`
+              );
+            }
+            if (part.types.includes("administrative_area_level_3")) {
+              document.body.insertAdjacentHTML(
+                "beforeend",
+                `<p>LEVEL 3: ${part.long_name}</p>`
+              );
+            }
+          });
+        })
+        .catch(err => console.warn(err.message));
 
 
 }
 }
-
-*/
---
+++ projects/lab-28-9-21/popup/script.js
@@ -0,0 +1,6 @@
+function received(tabs){
+  let currenTab = tabs[0];
+  chrome.tabs.sendMessage(currentTab.id, {msg:"It's, me the popup window"});
+}
+
+chrome.tabs.query({active:true,currentWindow: true}, 
