89c4f553e31aec1386b2e78ca6f5e49df811f8ee
nko2005 <nko2005@nyu.edu>
2021-11-09 15:47:43 +0300

    changed background color

--- projects/ourcode/server.js
+++ projects/ourcode/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3123
-//const port = process.env.PORT;
+//const port = 3123
+const port = process.env.PORT;
 app.use(express.static("public"));
 
 let counter = 0;
